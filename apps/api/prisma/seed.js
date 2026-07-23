428c30c5d513f3c7ce8289e8360b98db567d22c7
Ankit Soni <ankitks1515@gmail.com>
2026-07-23 14:56:11 +0530

    revert: remove dotenv import from seed.js

--- apps/api/prisma/seed.js
+++ apps/api/prisma/seed.js
@@ -1,4 +1,3 @@
-require('dotenv').config({ path: require('path').resolve(__dirname, '../.env') });
 const { PrismaClient } = require('@prisma/client');
 const { PrismaPg } = require('@prisma/adapter-pg');
 const { Pool } = require('pg');
